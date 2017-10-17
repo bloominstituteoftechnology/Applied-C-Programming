9c51567950ff91dbef8811ee7d67c173285c5745
manishavi <manishalal972@gmail.com>
2017-10-17 18:42:25 -0500

    triend to send response

--- assignment/request_parser.c
+++ assignment/request_parser.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define GET_ROOT "GET / H"
 #define GET_INFO "GET /info"
 #define POST_INFO "POST /info"
 #define GET_RESPONSE_HEAD "<html><head></head><body>Welcome to Manisha's Page</body></html>"
 #define HEADER_BODY_SEPERATOR "\r\n\r\n"
+#define CONTENT_LENGTH "Content-Length: "
+#define CONTENT_LENGTH_HEADER_LENGTH 16
 
 
 char* parse_client_request(const char* request, int size) {
@@ -45,6 +48,29 @@ char* parse_client_request(const char* request, int size) {
     if (find_POST != NULL) {
         puts("I found Post info!");
     }
+    char* find_Content_Length = strnstr(request, CONTENT_LENGTH, size);
+    char* find_len_cont_len = strnstr(find_Content_Length, "\n", size-strlen(find_Content_Length));
+    puts("Did we find Content-Length: here?");
+    printf("%s\n", find_Content_Length);
+    printf("%s\n", find_len_cont_len);
+    puts("Did we find next line here?");
+    printf("%p\n", find_Content_Length);
+    printf("%p\n", find_len_cont_len);
+
+    int len_con_len_line = find_len_cont_len - find_Content_Length-1;
+    char len_str[1024];
+    strncpy(len_str, find_Content_Length + CONTENT_LENGTH_HEADER_LENGTH, len_con_len_line - CONTENT_LENGTH_HEADER_LENGTH);
+    int body_length = atoi(len_str);
+    printf("Content Length? %d\n", body_length);
+    printf("Difference between two pointers: %ld\n", find_len_cont_len - find_Content_Length);
+
+    char* body = strnstr(request, HEADER_BODY_SEPERATOR, size);
+    printf("BODY! %s\n", body);
+
+    char the_body[1024];
+    memset(the_body, 0, sizeof(the_body));
+    strncpy(the_body, body+4, body_length);
+    printf("THE BODY: %s\n", the_body);
 
     return " ";
 }
--- assignment/server.c
+++ assignment/server.c
@@ -141,6 +141,8 @@ int main(void)
       close(sockfd); // child doesn't need the listener
       // LS: Send the correct response in JSON format
       // if (send(new_fd, "HTTP/1.0 200 OK\n\n<html><head></head><body>Hello World!</body></html>", 69, 0) == -1)
+      response = "Hello Friend, now you are viewing working webpage.\n";
+      write(new_fd, response, strlen(response));
       if (send(new_fd, response, strlen(response), 0) == -1)
         perror("send");
         close(new_fd);
