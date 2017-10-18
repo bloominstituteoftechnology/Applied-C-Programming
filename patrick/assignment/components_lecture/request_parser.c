322e507e0fc04693c2a50adb445d438c2b502fcc
mixelpixel <mixelpix@mac.com>
2017-10-18 14:30:18 -0400

    idem

--- patrick/assignment/components_lecture/request_parser.c
+++ patrick/assignment/components_lecture/request_parser.c
@@ -8,10 +8,21 @@
 #define GET_RESPONSE_HEAD "<html><head></head><body>Welcome to Patrick's AWESOME Page</body></html>"
 #define GET_RESPONSE_TAIL "\r\n\r\n"
 
+/******************************************************************************
+ * parse_client_request takes a single HTTP request as a STRING
+ * and PROCESSES it.
+ * NOTE: The 'request' char* could contain binary data, which we
+ * are not handling. If it did get binary data, it would crash (or worse).
+ ******************************************************************************/
 
 char* parse_client_request(const char* request, int size) {
     printf("Parsing data of size: %d\n", size);
     printf("Client opened socket and sent: \n\n%s\n", request);
+    // int double_newline = memchr(buffer, "\n", 1024);
+    // printf("read_result: %d\n", double_newline);
+
+// LS: loop above until \n\n is sent, signaling the end of an HTTP request
+
 // LS: parse the input and determine what result to send
 
 // 1: must handle GET/
